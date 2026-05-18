81af7d9cdd163d705fed541ac340c86493b327ae
sarbjot-arnifi <sarbjot@arnifi.com>
2026-05-18 17:48:40 +0530

    banner

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Navigation from "./_components/Navigation";
+import Banner from "./_components/Banner";
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
     <Navigation />
+    <Banner />
     </div>
   );
 }
