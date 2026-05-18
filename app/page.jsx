a4b355de41cc28deafa3d4bfd10e1a058467a8e0
sarbjot-arnifi <sarbjot@arnifi.com>
2026-05-18 17:58:36 +0530

    pricing section added

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,17 @@ import Image from "next/image";
 import Navigation from "./_components/Navigation";
 import Banner from "./_components/Banner";
 import Features from "./_components/Features";
+import HowItWorks from "./_components/HowItWorks";
+import Pricing from "./_components/Pricing";
+
 export default function Home() {
   return (
     <div>
     <Navigation />
     <Banner />
     <Features />
+    <HowItWorks />
+    <Pricing />
     </div>
   );
 }
